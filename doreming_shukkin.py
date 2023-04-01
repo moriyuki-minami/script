29c3ad92896d99d8b635e9e5b5ae6d508eaa8209
moriyuki-minami <111379871+moriyuki-minami@users.noreply.github.com>
2023-04-02 00:30:20 +0900

    Update doreming_shukkin.py

--- doreming_shukkin.py
+++ doreming_shukkin.py
@@ -16,7 +16,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class TestDoreming():
   def setup_method(self, method):
-    options = ChromeOptions()
+    options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     self.driver = webdriver.Chrome(options=options)
